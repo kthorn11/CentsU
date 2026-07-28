c9e31bc3a378eb92cd0828a44799746e7cfb04d7
kthorn11 <kaitmarie713@gmail.com>
2026-07-28 12:23:34 -0700

    Fixed tracker error

--- auth.js
+++ auth.js
@@ -47,6 +47,9 @@ signupButton.onclick = function () {
             .then(function () {
                 alert("Logged in successfully!");
 
+                document.getElementById("authSection").style.display = "none";
+                document.getElementById("expensrSection").style.display = "block";
+
                 email.value = "";
                 password.value = "";
             })
